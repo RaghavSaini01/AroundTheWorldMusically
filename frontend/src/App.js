968385e21dc3f95f26b027d97b57dc6b32bd2379
raghav3 <raghav3@illinois.edu>
2021-04-28 16:18:36 -0500

    Idek

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  
   const [genreName, setGenreName] =  useState('');
   const [genreList, setGenreList] = useState([]);
   const [artistName, setArtistName] =  useState('');
@@ -63,68 +64,71 @@ function App() {
   return (
 
     <div className="App">
-      <h1>Around the World - MUSICALLY</h1>
+      <h1>Around the World MUSICALLY</h1>
       <h2>Discover music by searching for your favorite song, and we we'll return some diverse new songs that match your preferences.</h2>
 
-    <div className="form">
-      <label> Add Genre Name:</label>
+    <div className="Input_Div">
+        <label> Add Genre Name: </label>
+        <input type="text" name="genreName" onChange={(e) => {
+          setGenreName(e.target.value)
+        } }/>
+        <br></br>
+        <br></br>
+        <button onClick={submitGenre}> Submit New Genre Name</button>
 
-      <input type="text" name="genreName" onChange={(e) => {
-        setGenreName(e.target.value)
-      } }/>
-      
-      <button onClick={submitGenre}> Submit New Genre Name</button>
-
-      <label>Find Genre Associated With Artist:</label>
-
-      <input type="text" name="artistName" onChange={(e) => {
-        setArtistName(e.target.value)
-      } }/>
-
-      <button onClick={findArtistGenre}> Submit Artist Name</button>
-
-      {genreList.map((val) => {
-        return (
-          <div className = "card">
-            <h1> GenreName: {val.genreName} </h1>
-            <button onClick={() => { deleteGenre(val.genreName) }}> Delete</button>
-            <input type="text" id="updateInput" onChange={(e) => {
-                setNewGenre(e.target.value)
-              } }/>
-              <button onClick={() => {
-                updateGenre(val.genreName)
-              }}> Update</button>
-            </div>
-        );
         
-      })}
-
-      <button onClick={() => {displaySrutiQueryy()}}> Run Sruti's Query</button>
-
-
-      {queriedGenreName.map((val) => {
-        return (
-          <div className = "card">
-            <h1> {val.artistName} genres: </h1>
-            { val.genres.map(genre => <h2> { genre.Genre_name } </h2> ) }
-            <button onClick={() => {setQueriedGenreName(queriedGenreName.filter((value, i, arr) => {return value !== val} ))} }>Delete Query</button>
+    {genreList.map((val) => {
+      return (
+        <div className = "card">
+          <h1> GenreName: {val.genreName} </h1>
+          <button onClick={() => { deleteGenre(val.genreName) }}> Delete</button>
+          <input type="text" id="updateInput" onChange={(e) => {
+              setNewGenre(e.target.value)
+            } }/>
+            <button onClick={() => {
+              updateGenre(val.genreName)
+            }}> Update</button>
           </div>
-        );
-        
-      })}
+      );
+          
+    })}
+    </div>
 
-      {srutiQueryName.map((val) => {
-        return (
-          <div className = "card">
-            { val.datas.map(data => <h3> Energy: { data.Energy }    |    Num Songs: {data.num_songs_with_high_energy_and_valance} </h3> ) }
-          </div>
-        );
-        
-      })}
+    <div className="Input_Div">
+    <label id="Genre_Label"> Find Genre Associated With Artist: </label>
+
+    <input type="text" name="artistName" onChange={(e) => {
+      setArtistName(e.target.value)
+    } }/>
+
+    <br></br>
+    <br></br>
+
+    <button id="Submit_Artist_Name" onClick={findArtistGenre}> Submit Artist Name</button>
+
+    <button id = "Query" onClick={() => {displaySrutiQueryy()}}> Get Artist Name</button>
+
+
+    {queriedGenreName.map((val) => {
+      return (
+        <div className = "card">
+          <h1> {val.artistName} Similar Songs: </h1>
+          { val.genres.map(genre => <h2> { genre.Song_name } </h2> ) }
+          <button onClick={() => {setQueriedGenreName(queriedGenreName.filter((value, i, arr) => {return value !== val} ))} }>Delete Query</button>
+        </div>
+      );
+    })}
+
+    {srutiQueryName.map((val) => {
+      return (
+        <div className = "card">
+          { val.datas.map(data => <h3> Energy: { data.Energy }    |    Num Songs: {data.num_songs_with_high_energy_and_valance} </h3> ) }
+        </div>
+      );
       
+    })}
 
     </div>
-    
   </div>
   );
 }
